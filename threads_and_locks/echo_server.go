f493a08859d085646fda0b56f0ca18ba3c0f946c
Adam Cardenas <cevaris@gmail.com>
2015-02-19 13:03:27 -0700

    got working executor service in go

--- threads_and_locks/echo_server.go
+++ threads_and_locks/echo_server.go
@@ -4,11 +4,38 @@ import (
 	"sync"
 	"net"
 	"regexp"
+	"runtime"
 )
 
 // Notes
 // - http://jan.newmarch.name/go/socket/chapter-socket.html#heading_toc_j_19
 // - http://stackoverflow.com/questions/18405023/how-would-you-define-a-pool-of-goroutines-to-be-executed-at-once-in-golang?answertab=votes#tab-top
+
+type ExecutorService struct {
+	MaxPoolSize int
+	Jobs chan *ConnectionHandler
+}
+
+func NewExecutorService(maxPoolSize int) *ExecutorService {
+	e := &ExecutorService{
+		MaxPoolSize: maxPoolSize,
+		Jobs: make(chan *ConnectionHandler),
+	}
+	for i:=0; i<e.MaxPoolSize; i++ {
+		go Worker(e.Jobs)
+	}
+	return e
+}
+
+func Worker(jobs chan *ConnectionHandler) {
+	for job := range jobs {
+		job.Run()
+	}
+}
+
+func (e *ExecutorService) execute(handler *ConnectionHandler) {
+	e.Jobs <- handler
+}
 
 type ConnectionHandler struct {
 	Conn net.Conn
@@ -16,8 +43,8 @@ type ConnectionHandler struct {
 }
 
 // ConnectionHandler constructor
-func NewConnectionHandler(conn net.Conn, pool *sync.WaitGroup) *ConnectionHandler {
-	return &ConnectionHandler{Conn: conn, Pool: pool}
+func NewConnectionHandler(conn net.Conn) *ConnectionHandler {
+	return &ConnectionHandler{Conn: conn}
 }
 
 func (h *ConnectionHandler) Run() {
@@ -25,8 +52,7 @@ func (h *ConnectionHandler) Run() {
 	// - Mark thread as done
 	// - Close connection to socket
 	defer h.Conn.Close()
-	defer h.Pool.Done()
-
+	
 	var buffer [1024]byte
 	for { // Run till recieve exit request
 		// Read in chunk of data
@@ -49,16 +75,16 @@ func (h *ConnectionHandler) Run() {
 }
 
 func main() {
-	var wg sync.WaitGroup
+
+	poolSize := runtime.NumCPU() * 2
+
+	executor := NewExecutorService(poolSize)
+
 	tcpAddr, _ := net.ResolveTCPAddr("tcp4", "localhost:4567")
 	listener, _ := net.ListenTCP("tcp", tcpAddr)
 	
 	for {
 		conn, _ := listener.Accept()
-		// Up the pool count
-		wg.Add(1)
-		// Invoke new thread to handle new connection
-		go NewConnectionHandler(conn, &wg).Run()
+		executor.execute(NewConnectionHandler(conn))
 	}
-	wg.Wait()
 }
